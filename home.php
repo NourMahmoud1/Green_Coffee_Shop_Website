9a8b40f07ccecd87ce89c71d0d2776c2669f646c
nour <nm704015@gmail.com>
2024-04-21 18:14:12 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section class="thumb">
+  <div class="box-container">
+    <div class="box">
+      <img src="img/thumb2.jpg" alt="">
+      <h3>green tea</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <i class="bx bx-chevron-right"></i>
+    </div>
+    <div class="box">
+      <img src="img/thumb0.jpg" alt="">
+      <h3>lemon coffee</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <i class="bx bx-chevron-right"></i>
+    </div>
+    <div class="box">
+      <img src="img/thumb1.jpg" alt="">
+      <h3>green tea</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <i class="bx bx-chevron-right"></i>
+    </div>
+    <div class="box">
+      <img src="img/thumb.jpg" alt="">
+      <h3>green tea</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <i class="bx bx-chevron-right"></i>
+    </div>
+  </div>
+  </section>
+</body>
+</html>
