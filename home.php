c9f2d9cd97ffd4df867d273fed1cec63a105d533
nour <nm704015@gmail.com>
2024-04-22 20:26:09 +0200

    complet the navbar and end in 41:54 minute

--- home.php
+++ home.php
@@ -1,39 +1,22 @@
+
+  <?php include 'componets/connection.php' ?>
+<style type="text/css">
+  <?php include 'style.css' ?>
+</style>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+  <title>Green coffee - home page</title>
 </head>
 <body>
-  <section class="thumb">
-  <div class="box-container">
-    <div class="box">
-      <img src="img/thumb2.jpg" alt="">
-      <h3>green tea</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <i class="bx bx-chevron-right"></i>
-    </div>
-    <div class="box">
-      <img src="img/thumb0.jpg" alt="">
-      <h3>lemon coffee</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <i class="bx bx-chevron-right"></i>
-    </div>
-    <div class="box">
-      <img src="img/thumb1.jpg" alt="">
-      <h3>green tea</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <i class="bx bx-chevron-right"></i>
-    </div>
-    <div class="box">
-      <img src="img/thumb.jpg" alt="">
-      <h3>green tea</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <i class="bx bx-chevron-right"></i>
-    </div>
-  </div>
-  </section>
+  <?php include 'componets/header.php' ?>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+  <script src="scrip.js"></script>
+  <?php include 'componets/alert.php' ?>
 </body>
 </html>
